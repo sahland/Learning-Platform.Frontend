3357a73c0444b83a7e90c60ca848b732cddbd69a
sahland <sahland@mail.ru>
2024-04-30 01:49:24 +0300

    feat(knitwit-93): add settings widget

---
+++ knitwit/lib/features/settings/view/settings_screen.dart
@@ -0,0 +1,50 @@
+import 'package:auto_route/auto_route.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+
+@RoutePage()
+class SettingsScreen extends StatelessWidget {
+  const SettingsScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final theme = Theme.of(context);
+
+    return Scaffold(
+      body: CustomScrollView(
+        slivers: [
+          SliverAppBar(
+            pinned: true,
+            snap: true,
+            floating: true,
+            elevation: 0,
+            expandedHeight: 65,
+            title: const Center(
+              child: Text(
+                'Настройки',
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  color: Color(0xFFA3ABC3),
+                  fontSize: 20,
+                  fontFamily: 'Roboto',
+                  fontWeight: FontWeight.w500,
+                  height: 0,
+                ),
+              ),
+            ),
+            leading: IconButton(
+              onPressed: ()  {
+                context.router.maybePop();
+              },
+              icon: SvgPicture.asset('./assets/icons/arrow_left_icon.svg'),
+            ),
+            backgroundColor: theme.primaryColor,
+            actions: [
+              SizedBox(width: 50, height: 50)
+            ],
+          )
+        ],
+      ),
+    );
+  }
+}
