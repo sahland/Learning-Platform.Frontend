71dc5f9ad3ce6f63c877e6f672610859f5f8c49c
sahland <sahland@mail.ru>
2024-04-30 01:48:59 +0300

    feat(knitwit-93): add profile redact  widget

---
+++ knitwit/lib/features/profileRedact/view/profile_redact_screen.dart
@@ -0,0 +1,44 @@
+import 'package:auto_route/auto_route.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/svg.dart';
+import 'package:knitwit/features/knitwit_title.dart';
+import 'package:knitwit/features/profileRedact/widgets/redact_window.dart';
+
+@RoutePage()
+class ProfileRedactScreen extends StatelessWidget {
+  const ProfileRedactScreen({super.key});
+  
+  @override
+  Widget build(BuildContext context) {
+    final theme = Theme.of(context);
+    
+    return Scaffold(
+      body: CustomScrollView(
+        slivers: [
+          SliverAppBar(
+            pinned: false,
+            snap: false,
+            floating: false,
+            expandedHeight: 75,
+            backgroundColor: theme.primaryColor,
+            automaticallyImplyLeading: false,
+            title: const KnitwitTitle(title: 'Редактировать профиль'),
+            leading: IconButton(
+              onPressed: () {
+                context.router.maybePop();
+              },
+              icon: SvgPicture.asset('./assets/icons/arrow_left_icon.svg'),
+            ),
+            actions: [
+              SizedBox(width: 50, height: 50,)
+            ],
+            elevation: 4,
+          ),
+          const SliverToBoxAdapter(
+            child: RedactWindow(),
+          )
+        ],
+      ),
+    );
+  }
+}
---
+++ knitwit/lib/features/profileRedact/widgets/redact_window.dart
@@ -0,0 +1,186 @@
+import 'package:flutter/material.dart';
+
+class RedactWindow extends StatefulWidget {
+  const RedactWindow({super.key});
+
+  @override
+  State<RedactWindow> createState() => _RedactWindowState();
+}
+
+class _RedactWindowState extends State<RedactWindow> {
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 0),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          const Center(
+            child: CircleAvatar(
+              radius: 63,
+              backgroundImage: AssetImage('./assets/images/user_avatar.jpg'),
+            ),
+          ),
+          const SizedBox(height: 20),
+          const Row(
+            children: [
+              Expanded(
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: [
+                      Text(
+                        'Username',
+                        style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 20,
+                            fontFamily: 'Roboto',
+                            fontWeight: FontWeight.w700
+                        ),
+                      ),
+                      SizedBox(height: 4),
+                    ],
+                  )
+              )
+            ],
+          ),
+          const SizedBox(height: 40),
+          ElevatedButton(
+            style: ElevatedButton.styleFrom(
+              backgroundColor: Colors.transparent,
+              elevation: 0,
+              shadowColor: Colors.transparent,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(0),
+              ),
+            ),
+            onPressed: () {},
+            child: Container(
+              width: double.infinity,
+              padding: EdgeInsets.zero,
+              child: const Align(
+                alignment: Alignment.centerLeft,
+                child: Text(
+                  'Смена аватара',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 16,
+                    fontFamily: 'Roboto',
+                    fontWeight: FontWeight.w500,
+                  ),
+                ),
+              ),
+            ),
+          ),
+          ElevatedButton(
+            style: ElevatedButton.styleFrom(
+              backgroundColor: Colors.transparent,
+              elevation: 0,
+              shadowColor: Colors.transparent,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(0),
+              ),
+            ),
+            onPressed: () {},
+            child: Container(
+              width: double.infinity,
+              padding: EdgeInsets.zero,
+              child: const Align(
+                alignment: Alignment.centerLeft,
+                child: Text(
+                  'Сменить никнейм',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 16,
+                    fontFamily: 'Roboto',
+                    fontWeight: FontWeight.w500,
+                  ),
+                ),
+              ),
+            ),
+          ),
+          ElevatedButton(
+            style: ElevatedButton.styleFrom(
+              backgroundColor: Colors.transparent,
+              elevation: 0,
+              shadowColor: Colors.transparent,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(0),
+              ),
+            ),
+            onPressed: () {},
+            child: Container(
+              width: double.infinity,
+              padding: EdgeInsets.zero,
+              child: const Align(
+                alignment: Alignment.centerLeft,
+                child: Text(
+                  'Смена почты',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 16,
+                    fontFamily: 'Roboto',
+                    fontWeight: FontWeight.w500,
+                  ),
+                ),
+              ),
+            ),
+          ),
+          ElevatedButton(
+            style: ElevatedButton.styleFrom(
+              backgroundColor: Colors.transparent,
+              elevation: 0,
+              shadowColor: Colors.transparent,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(0),
+              ),
+            ),
+            onPressed: () {},
+            child: Container(
+              width: double.infinity,
+              padding: EdgeInsets.zero,
+              child: const Align(
+                alignment: Alignment.centerLeft,
+                child: Text(
+                  'Сброс пароля',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 16,
+                    fontFamily: 'Roboto',
+                    fontWeight: FontWeight.w500,
+                  ),
+                ),
+              ),
+            ),
+          ),
+          ElevatedButton(
+            style: ElevatedButton.styleFrom(
+              backgroundColor: Colors.transparent,
+              elevation: 0,
+              shadowColor: Colors.transparent,
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(0),
+              ),
+            ),
+            onPressed: () {},
+            child: Container(
+              width: double.infinity,
+              padding: EdgeInsets.zero,
+              child: const Align(
+                alignment: Alignment.centerLeft,
+                child: Text(
+                  'Выйти из аккаунта',
+                  style: TextStyle(
+                    color: Color(0xFFBE1257),
+                    fontSize: 16,
+                    fontFamily: 'Roboto',
+                    fontWeight: FontWeight.w500,
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
