b0ab935565c60d587361cc3babad2a790d050fef
sahland <sahland@mail.ru>
2024-05-21 21:37:26 +0300

    chore(knitwit-102): update main file

--- knitwit/lib/main.dart
+++ knitwit/lib/main.dart
@@ -1,8 +1,13 @@
+import 'package:appmetrica_plugin/appmetrica_plugin.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:knitwit/router/router.dart';
 
 Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await dotenv.load(fileName: '.env');
   runApp(const KnitwitApp());
+  _initAppMetric();
 }
 
 class KnitwitApp extends StatefulWidget {
@@ -13,13 +18,18 @@ class KnitwitApp extends StatefulWidget {
   State<KnitwitApp> createState() => _KnitwitApp();
 }
 
+Future<void> _initAppMetric() async {
+  //final appmetric_key = dotenv.env['APPMETRICA_KEY'];
+
+  //print(appmetric_key);
+}
+
 class _KnitwitApp extends State<KnitwitApp> {
   final _router = AppRouter();
 
   @override
   Widget build(BuildContext context) {
     final primaryColor = Color(0xFF1C202A);
-
     return MaterialApp.router(
       title: 'KnitWit',
       theme: ThemeData(
