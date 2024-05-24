1e73cefe7c1d5dfe07aaecd601473545543e815e
sahland <sahland@mail.ru>
2024-05-24 19:36:54 +0300

    chore(knitwit-102): update api file

--- knitwit/lib/api/api.dart
+++ knitwit/lib/api/api.dart
@@ -1,3 +1,8 @@
 export 'courses/courses.dart';
-export 'sections/sections.dart';
 export 'models/models.dart';
+export 'courseRatings/course_ratings.dart';
+export 'courseSections/course_sections.dart';
+export 'learningProgresses/learning_progresses.dart';
+export 'notifications/notifications.dart';
+export 'tags/tags.dart';
+export 'users/users.dart';
