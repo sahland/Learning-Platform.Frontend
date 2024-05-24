afc86851a3d72bb4bba3e1ce4bf95832c0b2960e
sahland <sahland@mail.ru>
2024-05-24 19:33:51 +0300

    chore(knitwit-102): create notifications api

---
+++ knitwit/lib/api/notifications/notifications_api_client.dart
@@ -0,0 +1,28 @@
+import 'package:dio/dio.dart';
+import 'package:retrofit/http.dart';
+import '../models/models.dart';
+
+part 'notifications_api_client.g.dart';
+
+@RestApi(baseUrl: '')
+abstract class NotificationsApiClient {
+  factory NotificationsApiClient(Dio dio, {String baseUrl}) = _NotificationsApiClient;
+
+  factory NotificationsApiClient.create({String? apiUrl}) {
+    final dio = Dio();
+    if (apiUrl != null) {
+      return NotificationsApiClient(dio, baseUrl: apiUrl);
+    }
+    return NotificationsApiClient(dio);
+  }
+
+  @GET('/api/v1/notifications/{notificationsId}')
+  Future<Notification> getNotificationById(
+    @Query('notificationId') int notificationId,
+  );
+
+  @GET('/api/v1/notifications/all')
+  Future<Notifications> getNotifications(
+  );
+
+}
