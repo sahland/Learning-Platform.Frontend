b4af3544b95fbee0c1b0c00da966df7e510a8ace
sahland <sahland@mail.ru>
2024-06-16 15:09:57 +0300

    chore(knitwit-111): update main app files

---
+++ knitwit/lib/knitwit_app.dart
@@ -0,0 +1,83 @@
+import 'dart:developer';
+
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+import 'package:knitwit/api/api.dart';
+import 'package:knitwit/api/tags/tags_api_client.dart';
+import 'package:knitwit/features/auth/bloc/auth_bloc.dart';
+import 'package:knitwit/features/explore/bloc/bloc.dart';
+import 'package:knitwit/features/notifications/bloc/bloc.dart';
+import 'package:knitwit/features/profile/bloc/profile_bloc.dart';
+import 'package:knitwit/router/router.dart';
+
+class KnitwitApp extends StatefulWidget {
+  const KnitwitApp({super.key});
+
+  @override
+  State<KnitwitApp> createState() => _KnitwitAppState();
+}
+
+class _KnitwitAppState extends State<KnitwitApp> {
+  final storage = const FlutterSecureStorage();
+  final _router = AppRouter();
+  final coursesApiClient = CoursesApiClient.create(apiUrl: dotenv.env['API_KEY']);
+  final tagsApiClient = TagsApiClient.create(apiUrl: dotenv.env['API_KEY']);
+  final courseRatingsApiClient = CourseRatingsApiClient.create(apiUrl: dotenv.env['API_KEY']);
+  final courseSectionsApiClient = CourseSectionsApiClient.create(apiUrl: dotenv.env['API_KEY']);
+  final learningProgressesApiClient = LearningProgressesApiClient.create(apiUrl: dotenv.env['API_KEY']);
+  final notificationsApiClient = NotificationsApiClient.create(apiUrl: dotenv.env['API_KEY']);
+  final usersApiClient = UsersApiClient.create(apiUrl: dotenv.env['API_KEY']);
+  final authControllerApiClient = AuthControllerApiClient.create(apiUrl: dotenv.env['API_KEY']);
+
+  @override
+  Widget build(BuildContext context) {
+    log('${dotenv.env['API_KEY']}');
+    final primaryColor = Color(0xFF1C202A);
+
+    return MultiBlocProvider(
+      providers: [
+        BlocProvider(
+          create: (context) {
+            final exploreBloc = ExploreBloc(
+              coursesApiClient: coursesApiClient,
+              tagsApiClient: tagsApiClient,
+            );
+            exploreBloc.add(const ExploreInit());
+            return exploreBloc;
+          },
+        ),
+        BlocProvider(
+          create: (context) => NotificationBloc(
+            notificationsApiClient: notificationsApiClient,
+          ),
+        ),
+        BlocProvider(
+          create: (context) => ProfileBloc(
+            usersApiClient: usersApiClient,
+            storage: storage,
+          ),
+        ),
+        BlocProvider(
+          create: (context) => AuthBloc(
+            authControllerApiClient: authControllerApiClient,
+            usersApiClient: usersApiClient,
+            storage: storage,
+          ),
+        ),
+      ],
+      child: MaterialApp.router(
+        debugShowCheckedModeBanner: false,
+        title: 'KnitWit',
+        theme: ThemeData(
+          primaryColor: primaryColor,
+          scaffoldBackgroundColor: primaryColor,
+          colorScheme: ColorScheme.fromSeed(seedColor: primaryColor),
+          useMaterial3: true,
+        ),
+        routerConfig: _router.config(),
+      ),
+    );
+  }
+}
--- knitwit/lib/main.dart
+++ knitwit/lib/main.dart
@@ -1,46 +1,19 @@
 import 'package:appmetrica_plugin/appmetrica_plugin.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_dotenv/flutter_dotenv.dart';
-import 'package:knitwit/router/router.dart';
+import 'package:knitwit/knitwit_app.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await dotenv.load(fileName: '.env');
-  runApp(const KnitwitApp());
   _initAppMetric();
-}
-
-class KnitwitApp extends StatefulWidget {
-  const KnitwitApp({super.key});
-
-
-  @override
-  State<KnitwitApp> createState() => _KnitwitApp();
+  runApp(const KnitwitApp());
 }
 
 Future<void> _initAppMetric() async {
-  //final appmetric_key = dotenv.env['APPMETRICA_KEY'];
-
-  //print(appmetric_key);
+  final appmetric_key = dotenv.env['APPMETRICA_KEY'].toString();
+  AppMetrica.activate(AppMetricaConfig(appmetric_key, logs: true));
 }
 
-class _KnitwitApp extends State<KnitwitApp> {
-  final _router = AppRouter();
-
-  @override
-  Widget build(BuildContext context) {
-    final primaryColor = Color(0xFF1C202A);
-    return MaterialApp.router(
-      title: 'KnitWit',
-      theme: ThemeData(
-        primaryColor: primaryColor,
-        scaffoldBackgroundColor: primaryColor,
-        colorScheme: ColorScheme.fromSeed(seedColor: primaryColor),
-        useMaterial3: true,
-      ),
-      routerConfig: _router.config(),
-    );
-  }
-}
 
 
