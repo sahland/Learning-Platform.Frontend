6c3ce4a05b1b3a522b2d32004f77721848ab2eff
sahland <sahland@mail.ru>
2024-06-16 15:05:25 +0300

    chore(knitwit-111): update profile widgets

--- knitwit/lib/features/profile/view/profile_screen.dart
+++ knitwit/lib/features/profile/view/profile_screen.dart
@@ -1,21 +1,60 @@
 import 'package:appmetrica_plugin/appmetrica_plugin.dart';
 import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_svg/svg.dart';
-import 'package:knitwit/features/profile/widgets/profile_user.dart';
+import 'package:knitwit/api/api.dart';
+import 'package:knitwit/features/profile/bloc/bloc.dart';
+import 'package:knitwit/features/profile/bloc/profile_bloc.dart';
+import '../../../common/common.dart';
 import 'package:knitwit/router/router.dart';
-
-import '../../../common/widgets/knitwit_title.dart';
+import '../widgets/widgets.dart';
 
 @RoutePage()
-class ProfileScreen extends StatelessWidget {
+class ProfileScreen extends StatefulWidget {
   const ProfileScreen({super.key});
 
   @override
-  Widget build(BuildContext context){
+  State<ProfileScreen> createState() => _ProfileScreenState();
+}
+
+class _ProfileScreenState extends State<ProfileScreen> {
+  @override
+  void initState() {
+    BlocProvider.of<ProfileBloc>(context).add(const ProfileLoadEvent());
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
     final theme = Theme.of(context);
     AppMetrica.reportEvent('Viewing the profile');
+    return BlocListener<ProfileBloc, ProfileState>(
+      listener: (context, state) {
+        if (state is ProfileFailure) {
+          _refreshToken(context);
+        }
+      },
+      child: BlocBuilder<ProfileBloc, ProfileState>(
+        builder: (context, state) {
+          if (state is ProfileLoadedState) {
+            final user = state.user;
+            return _buildContext(user, theme, context);
+          } else if (state is ProfileFailure) {
+            return Center(
+              child: Text(
+                'Failed to load profile: ${state.error}',
+                style: TextStyle(color: Colors.red),
+              ),
+            );
+          }
+          return _buildLoading();
+        },
+      ),
+    );
+  }
 
+  Scaffold _buildContext(User user, ThemeData theme, BuildContext context) {
     return Scaffold(
       body: CustomScrollView(
         slivers: [
@@ -26,7 +65,7 @@ class ProfileScreen extends StatelessWidget {
             expandedHeight: 75,
             backgroundColor: theme.primaryColor,
             centerTitle: true,
-            title: const KnitwitTitle(title: 'Мой профиль'),
+            title: const Text('Мой профиль'),
             leading: IconButton(
               onPressed: () {
                 context.router.push(const NotificationsRoute());
@@ -49,11 +88,27 @@ class ProfileScreen extends StatelessWidget {
             ],
             elevation: 4,
           ),
-           SliverToBoxAdapter(
-            child: ProfileUsers(),
+          SliverToBoxAdapter(
+            child: ProfileUser(
+              username: user.username,
+              userId: user.userId.toString(),
+            ),
           ),
         ],
       ),
     );
+  }
+
+  Scaffold _buildLoading() => const Scaffold(
+    body: Center(
+      child: CircularProgressIndicator(
+        color: Color(0xFFFBE1257),
+      ),
+    ),
+  );
+
+  void _refreshToken(BuildContext context) {
+    final profileBloc = BlocProvider.of<ProfileBloc>(context);
+    profileBloc.add(const ProfileLoadEvent());
   }
 }
--- knitwit/lib/features/profile/widgets/profile_user.dart
+++ knitwit/lib/features/profile/widgets/profile_user.dart
@@ -1,21 +1,22 @@
 import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
-import 'package:knitwit/common/widgets/base_long_button.dart';
 import 'package:knitwit/router/router.dart';
+import '../../../common/common.dart';
 
-class ProfileUsers extends StatefulWidget {
+class ProfileUser extends StatelessWidget {
+  const ProfileUser({
+    super.key,
+    this.username,
+    this.userId,
+  });
 
-  const ProfileUsers({super.key,});
+  final String? username;
+  final String? userId;
 
-  @override
-  State<ProfileUsers> createState() => _ProfileUsersState();
-}
-
-class _ProfileUsersState extends State<ProfileUsers>{
   @override
   Widget build(BuildContext context) {
     return Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 0),
+      padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 0),
       child: Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
@@ -26,97 +27,96 @@ class _ProfileUsersState extends State<ProfileUsers>{
             ),
           ),
           const SizedBox(height: 20),
-          const Row(
+          Row(
             children: [
               Expanded(
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    children: [
-                      Text(
-                          'Username',
-                        style: TextStyle(
-                          color: Colors.white,
-                          fontSize: 20,
-                          fontFamily: 'Roboto',
-                          fontWeight: FontWeight.w700
-                        ),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Text(
+                      username ?? '',
+                      style: const TextStyle(
+                        color: Colors.white,
+                        fontSize: 20,
+                        fontFamily: 'Roboto',
+                        fontWeight: FontWeight.w700,
                       ),
-                      SizedBox(height: 4),
-                      Text(
-                        'User ID: 1234567890',
-                        style: TextStyle(
-                            color: Color(0xFFA4ACC3),
-                            fontSize: 12,
-                            fontFamily: 'Roboto',
-                            fontWeight: FontWeight.w500
-                        ),
+                    ),
+                    const SizedBox(height: 4),
+                    Text(
+                      'User ID: ${userId ?? ''}',
+                      style: const TextStyle(
+                        color: Color(0xFFA4ACC3),
+                        fontSize: 12,
+                        fontFamily: 'Roboto',
+                        fontWeight: FontWeight.w500,
                       ),
-                    ],
-                  )
+                    ),
+                  ],
+                ),
               )
             ],
           ),
           const SizedBox(height: 10),
           Center(
-            child:
-            ElevatedButton(
+            child: ElevatedButton(
               style: ElevatedButton.styleFrom(
                 backgroundColor: const Color(0xFFBE1257),
                 shape: RoundedRectangleBorder(
                   borderRadius: BorderRadius.circular(8),
-                )
+                ),
               ),
-              onPressed:() {
+              onPressed: () {
                 context.router.push(const ProfileRedactRoute());
               },
               child: const Text(
-                  'Редактировать профиль',
-                  style: TextStyle(
-                    color: Colors.white,
-                    fontSize: 14,
-                    fontFamily: 'Roboto',
-                    fontWeight: FontWeight.w500,
-                  ),
+                'Редактировать профиль',
+                style: TextStyle(
+                  color: Colors.white,
+                  fontSize: 14,
+                  fontFamily: 'Roboto',
+                  fontWeight: FontWeight.w500,
+                ),
               ),
             ),
           ),
           const SizedBox(height: 10),
           DecoratedBox(
-              decoration: const BoxDecoration(
-                border: Border(
-                  bottom: BorderSide(
-                    color: Colors.white,
-                    width: 0.5,
-                  )
-                )
+            decoration: const BoxDecoration(
+              border: Border(
+                bottom: BorderSide(
+                  color: Colors.white,
+                  width: 0.5,
+                ),
               ),
+            ),
             child: Container(
               height: 6.0,
             ),
           ),
           const SizedBox(height: 10),
           BaseLongButton(
-              onPressed: () async =>
-                await context.router.push(const FavoriteCoursesRoute()),
-              title: 'Отслеживаемые курсы',
-              textColor: Colors.white,
+            onPressed: () async =>
+              await context.router.push(const FavoriteCoursesRoute()),
+            title: 'Отслеживаемые курсы',
+            textColor: Colors.white,
           ),
           BaseLongButton(
-              onPressed: () async =>
-                await context.router.push(const CompletedCourseRoute()),
-              title: 'Пройденные курсы',
-              textColor: Colors.white,
+            onPressed: () async =>
+              await context.router.push(const CompletedCourseRoute()),
+            title: 'Пройденные курсы',
+            textColor: Colors.white,
           ),
           BaseLongButton(
-              onPressed: () async =>
-                await context.router.push(const EditorRoute()),
-              title: 'Создать курсы',
-              textColor: Colors.white,
+            onPressed: () async =>
+              await context.router.push(const EditorRoute()),
+            title: 'Создать курсы',
+            textColor: Colors.white,
           ),
           BaseLongButton(
-              onPressed: () async =>
-                await context.router.push(const MyCoursesRoute()),
-              title: 'Созданные мной курсы',
+            onPressed: () async =>
+              await context.router.push(const MyCoursesRoute()),
+            title: 'Созданные мной курсы',
             textColor: Colors.white,
           ),
           ElevatedButton(
@@ -151,4 +151,3 @@ class _ProfileUsersState extends State<ProfileUsers>{
     );
   }
 }
-
