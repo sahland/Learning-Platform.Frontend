0922736edb00691e5fef2b6f47ea7f1fda0bb626
sahland <sahland@mail.ru>
2024-06-16 15:07:02 +0300

    chore(knitwit-111): update features file

---
+++ knitwit/lib/features/features.dart
@@ -0,0 +1,15 @@
+export 'completed_courses/completed_courses.dart';
+export 'course/course.dart';
+export 'editor/editor.dart';
+export 'explore/explore.dart';
+export 'favorite_courses/favorite_courses.dart';
+export 'first/first.dart';
+export 'home/home.dart';
+export 'module/module.dart';
+export 'my_courses/my_courses.dart';
+export 'notifications/notifications.dart';
+export 'profile/profile.dart';
+export 'profile_redact/profile_redact.dart';
+export 'redact_module/redact_module.dart';
+export 'settings/settings.dart';
+export 'auth/auth.dart';
